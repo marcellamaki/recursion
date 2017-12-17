b1143cd9ef9d59f557876797bde2381b8fcc26b0
Marcella Maki <marcellamaki@gmail.com>
2017-12-17 16:22:52 -0500

    add reverse string with recursion example

--- recursion.js
+++ recursion.js
@@ -1,3 +1,4 @@
+//factorial
 function factorial(n) {
  if (n < 0) {
     return - 1;
@@ -6,4 +7,13 @@ function factorial(n) {
     return 1;
   }
   return n * factorial(n - 1);
+}
+
+
+//reverse string
+function reverseString(string) {  
+   if ( str.length <= 1 ) {    
+          return str;  
+   }  
+      return reverse(string.substr(1)) + string[ 0 ];
 }
